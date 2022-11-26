3cd0c73a28615095b1e3e56777fac4e90fd97770
B4kedBr3ad <110211701+B4kedBr3ad@users.noreply.github.com>
2022-11-27 00:04:23 +0900

    Add config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+exports.auth = {
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: '',
+};
+
+exports.blacklist_search_query = [
+  'marijuana',          // you can use words and hashtag
+  '#MDMA'
+];
