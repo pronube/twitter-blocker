f7775e3618815ea0d0f526a4744cabdb720d89c7
B4kedBr3ad <110211701+B4kedBr3ad@users.noreply.github.com>
2022-11-27 00:23:10 +0900

    Delete start.sh echo " directory

--- start.sh echo "/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash/
-echo "| installing packages"
-npm i twitter
-echo "| loading followers"
-node follow.js
-echo "| loaded followers"
-echo "|"
-echo "| start blocking!"
-node index.js  #while true; do node .; done
