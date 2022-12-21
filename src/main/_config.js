25a4d1e3534ec9a930a831b6214d6395e2c58aed
B4kedBr3ad <110211701+B4kedBr3ad@users.noreply.github.com>
2022-12-21 16:54:11 +0000

    Delete: config.js Delete: load_following.js Make: /src

---
+++ src/main/_config.js
@@ -0,0 +1,11 @@
+exports.auth = {
+  consumer_key: 'your_consumer_key',
+  consumer_secret: 'your_consumer_key_seacret',
+  access_token_key: 'your_access_token_key',
+  access_token_secret: 'your_access_token_key_seacret',
+};
+
+exports.blacklist_search_query = [
+  'NFT',
+  '#bitcoin'
+];
